3816fd844f1e1711cd65a02a4232d07395863cb6
Alexa Wu <alexawu12@gmail.com>
2026-09-23 13:37:03 -0700

    Create Library Class

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
 public class Library {
-
+	private ArrayList<Book> books;
+	private int numBooks;
+	
+	Library() {
+		books = new ArrayList<>();
+		numBooks = 0;
+	}
+	
+	public void addBook(Book book) {
+		books.add(book);
+		numBooks++;
+	}
+	
+	public void displayAllBooks() {
+		for(Book book : books) {
+			System.out.println(book);
+		}
+	}
+	
+	public Book findBookByTitle(String title) {
+		for(int i = 0; i < numBooks; i++) {
+			if(title.toLowerCase().equals(books.get(i).getTitle().toLowerCase())) {
+				return books.get(i);
+			}
+		}
+		return null;
+	}
 }

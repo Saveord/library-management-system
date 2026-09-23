8d377c98e5cb7b9773e7bde2e9639b09c4985e92
Alexa Wu <alexawu12@gmail.com>
2026-09-23 12:53:32 -0700

    Create Book Class

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,47 @@
+import java.time.LocalDate;
+
 
 public class Book {
+	
+	private String title; private String author;
+	private LocalDate publicationDate;
 
+	public Book() {
+	}
+
+	public Book(String title, String author, LocalDate publicationDate) {
+		this.title = title;
+		this.author = author;
+		this.publicationDate = publicationDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	// Getter for author
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public LocalDate getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(LocalDate publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "Book [Title=" + title + ", Author=" + author + ", Publication=" + publicationDate + "]";
+	}
 }
